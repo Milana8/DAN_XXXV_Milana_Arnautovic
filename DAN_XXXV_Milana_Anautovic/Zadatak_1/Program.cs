3c5bc876ee453ae2114bf008fae2dbd9be2fb7bb
Milana8 <91amilana@gmail.com>
2020-06-19 15:16:04 +0200

    resricting the user when entering a guess number

--- DAN_XXXV_Milana_Anautovic/Zadatak_1/Program.cs
+++ DAN_XXXV_Milana_Anautovic/Zadatak_1/Program.cs
@@ -13,6 +13,7 @@ namespace Zadatak_1
         static int number;
         static int Guess;
         public static Random random = new Random();
+
         /// <summary>
         /// Method for guessing numbers
         /// </summary>
@@ -25,25 +26,28 @@ namespace Zadatak_1
                 lock (l)
                 {
                     Guess = random.Next(1, 101);
-                    if ((Guess % 2 == 0 && number % 2 == 0) || (Guess % 2 == 1 && number % 2 == 1))
+                    if (Guess != number)
                     {
-                        Console.WriteLine(t.Name + " chose the number " + Guess + ". Wrong chois. The participant hit parity.");
-                    }
+                        if ((Guess % 2 == 0 && number % 2 == 0) || (Guess % 2 == 1 && number % 2 == 1))
+                        {
+                            Console.WriteLine(t.Name + " chose the number " + Guess + ". Wrong chois. The participant hit parity.");
+                        }
 
+                        else
+                        {
+                            Console.WriteLine(t.Name + " chose the number " + Guess + ". Wrong chois.");
+                        }
+
+                    }
                     else
-                    {
-                        Console.WriteLine(t.Name + " chose the number " + Guess + ". Wrong chois.");
-                    }
-
-
-                    while (Guess == number)
                     {
 
                         Console.WriteLine("Well done " + t.Name + "! The requested number is " + number);
                         Console.ReadLine();
                     }
+                    Thread.Sleep(100);
                 }
-                Thread.Sleep(100);
+                
             }
         }
 
@@ -78,15 +82,27 @@ namespace Zadatak_1
 
         static void Main(string[] args)
         {
-
+            bool guessBool;
             Console.WriteLine("***WELCOME***\n");
             Console.WriteLine("Enter the number of game participants");
             numUsers = AuxiliaryClass.ReadInteger();
-            Console.WriteLine("Enter the number to quess");
-            number = AuxiliaryClass.ReadInteger();
-            Console.WriteLine("The user has selected the number of participants. That number is " + numUsers + "\nThe number to guess was selected.\n");
+            do
+            {
+                Console.Write("Enter the number needed to guess (1 to 100): ");
+                string guessNumber = Console.ReadLine();
+               
+                guessBool = int.TryParse(guessNumber, out number);
+                if (number < 1 || number >= 100)
+                {
+                    
+                   Console.WriteLine("Please enter a number between 1 and 100.");
+                   guessBool = false;
+                }
+            } while (!guessBool);
+            
 
-            Thread tg = new Thread(() => ThreadGenerator());
+            Thread tg = new Thread(() => ThreadGenerator()); //creating thread
+            tg.Name = "Thread_Generator"; //naming threads
             tg.Start();
             tg.Join();
 
